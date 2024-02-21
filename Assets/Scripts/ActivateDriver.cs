bd219cce845d0c77b27430592a8cbf41e311ee6f
Kvan7 <71402892+Kvan7@users.noreply.github.com>
2024-02-21 15:50:58 -0600

    temp push

---
+++ Assets/Scripts/ActivateDriver.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateDriver : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public GameObject ventDriver;
+	public GameObject shelfDriver;
+	void Start()
+	{
+		ventDriver.SetActive(false);
+	}
+
+	public void ShowDriver()
+	{
+		if (!shelfDriver.activeSelf)
+		{
+			ventDriver.SetActive(true);
+		}
+	}
+}
